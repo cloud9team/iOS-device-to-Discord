186e26191eb08c350423eaffc0450c1e66020cee
cloud9team <8thpharoahgold@gmail.com>
2019-11-27 02:57:03 -0500

    Extended current commands. Added app aliasing in config. More to come.

--- config.example.py
+++ config.example.py
@@ -1,11 +1,35 @@
+# save this file as config.py
+
 bot_token = 'insert_token'
 bot_channel = 'channel_id'
 bot_prefix = '!'
 
+# replace devicename and uuid for each device
 devices={'devicename1':'uuid1',
     'devicename2':'uuid2',
     'devicename3':'uuid3'
     }
+    
+#    
+app_alias={'app1':'app.bundle.identifier',
+    'app2':'app2.bundle.identifier'
 
-# replace devicename and uuid for each device
-# save this file as config.py
+    }
+
+# Optional MYSQL Database Settings.
+# if DB_USER = 'user' and DB_PASSWORD = 'ABCD1234' MySQL functionality is off.
+DB_NAME = 'db'
+
+DB_HOST = 'port9000'
+
+DB_USER = 'user'
+
+DB_PASSWORD = 'abcd1234'
+
+# srSQl1 will run a conitnuous background task. query_interval in seconds.
+query_interval = 120
+strSQL1 = ("SELECT * FROM tblname")
+
+# strSQL2 works with query command. you can define multiple queries to run on demand. syntax !query strSQL2
+
+strSQL2 = ''
--- deviceM2.py
+++ deviceM2.py
@@ -4,8 +4,9 @@ import subprocess
 from discord.utils import get
 import os, fnmatch
 from discord.ext import commands
-from config import bot_token, devices, bot_channel, bot_prefix
-#import json
+from datetime import datetime as d
+from config import bot_token, devices, bot_channel, bot_prefix, app_alias
+import json
 
 TOKEN = bot_token
 
@@ -25,42 +26,101 @@ async def on_ready():
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
-    print('------')
+    print('------')    
+    await bot.change_presence(game=discord.Game(name='with all the connections.'),
+                                 status=discord.Status('online'))
+    msg = "**I'm alive again.**"
+    await bot.send_message(discord.Object(id=bot_channel), msg)
 
+#@bot.command(pass_context=True)
+#async def ping(ctx):
+#    start = d.timestamp(d.now())
+#    message = bot.send_message(discord.Object(id=bot_channel), content='Pinging')
+#    await message.edit(msg, content='Pong!\nPing time: {( d.timestamp( d.now() ) - start ) * 1000 }ms.')
 
 @bot.command(pass_context=True)
-async def help(ctx):
-    msg = "!delete <device_name> - delete RDM-UIC from device\n!listapps <device_name> - list apps on device\n!mac - screenshot of computer\n!reboot <device_name> - reboot device\n!sc <device_name> - screenshot of device"
+async def help(ctx, *, cmd: None):
+    cmd_help = str(cmd)
+    if cmd_help is not None:
+       if cmd_help == 'reboot':
+           msg = "(p)reboot <device_name> or <all> Ex: !reboot device1 - reboots device1."       
+       elif cmd_help == 'delete':
+           msg = ("(p)delete [<device_name> | <all>] <app_alias> Ex: !delete device1 app3 - Delete app3 from device1." +
+               "\nReplace device1 with all to delete app3 from all devices.")       
+       elif cmd_help == 'sc':
+           msg = "(p)sc <device_name> Ex: !sc device1 - Upload screen capture of device1."              
+       elif cmd_help == 'mac':
+           msg = "(p)mac Ex: !mac - Upload screen capture of Mac connected to devices."                     
+       else:
+           msg = "No command named " + cmd_help + " found."
+    else:       
+        msg = ("Type (p)help <commmand> for specific command help\n!delete <device_name> <app_alias> - delete app from device" +
+            "\n!listapps <device_name> - list apps on device" +
+            "\n!mac - screenshot of computer\n!reboot [<device_name> | <all>] - reboot single device or all" +
+            "\n!sc <device_name> - screenshot of device")
     await bot.send_message(discord.Object(id=bot_channel), msg )
 
 @bot.command(pass_context=True)
 async def reboot(ctx, device):
+    """(p)reboot <device_name> - reboot device. Replace <device_name> with <all> to reboot all devices."""
     name = str(device)
-    MyOut = subprocess.Popen(['idevicediagnostics', '-u', devices.get(name), 'restart'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-    stdout,stderr = MyOut.communicate()
-    await bot.send_message(discord.Object(id=bot_channel), stdout.decode("utf-8") )
-
+    if name == 'all':
+        for d in devices:
+            dname = devices[d]
+            MyOut = subprocess.Popen(['idevicediagnostics', '-u', dname, 'restart'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+            stdout,stderr = MyOut.communicate()
+            await bot.send_message(discord.Object(id=bot_channel), stdout.decode("utf-8") )
+    else:        
+        try:
+            MyOut = subprocess.Popen(['idevicediagnostics', '-u', devices.get(name), 'restart'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+            stdout,stderr = MyOut.communicate()
+            await bot.send_message(discord.Object(id=bot_channel), stdout.decode("utf-8") )
+        except:
+            await bot.send_message(discord.Object(id=bot_channel), "No device named " + name + " found.")
+            
 @bot.command(pass_context=True)
 async def sc(ctx, device):
-    # take screenshot of device
+    """(p)sc <device_name> - Get screen capture of device and upload to channel."""
     name = str(device)
-    MyOut = subprocess.Popen(['idevicescreenshot', '-u', devices.get(name), device+'.png'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-    stdout = MyOut.communicate()
-    await bot.send_file(discord.Object(id=bot_channel), name+'.png')
-
+    try:
+        MyOut = subprocess.Popen(['idevicescreenshot', '-u', devices.get(name), device+'.png'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+        stdout = MyOut.communicate()
+        await bot.send_file(discord.Object(id=bot_channel), name+'.png')
+    except:
+        await bot.send_message(discord.Object(id=bot_channel), "No device named " + name + " found.")
+    
 @bot.command(pass_context=True)
 async def listapps(ctx, device):
     name = str(device)
-    MyOut = subprocess.Popen(['ideviceinstaller', '-u', devices.get(name), '--list-apps'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-    stdout,stderr = MyOut.communicate()
-    await bot.send_message(discord.Object(id=bot_channel), stdout.decode("utf-8") )
-
+    try:
+        MyOut = subprocess.Popen(['ideviceinstaller', '-u', devices.get(name), '--list-apps'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+        stdout,stderr = MyOut.communicate()
+        await bot.send_message(discord.Object(id=bot_channel), stdout.decode("utf-8") )
+    except:
+        await bot.send_message(discord.Object(id=bot_channel), "No device named " + name + " found.")
+    
 @bot.command(pass_context=True)
-async def delete(ctx, device):
+async def delete(ctx, device, app):
+    """(p)delete <device_name> <app_alias> - Delete app from device using alias defined in config. Replace <device_name> with <all> to delete app from all devices."""
+
     name = str(device)
-    MyOut = subprocess.Popen(['ideviceinstaller', '-u', devices.get(name), '--uninstall', 'com.apple.test.RealDeviceMap-UIControlUITests-Runner'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-    stdout,stderr = MyOut.communicate()
-    await bot.send_message(discord.Object(id=bot_channel), stdout.decode("utf-8") )
+    alias = app_alias[app]
+    try:
+        if name == 'all':
+            for d in devices:
+                dname = devices[d]
+                print(dname)
+                MyOut = subprocess.Popen(['ideviceinstaller', '-u', dname, '--uninstall',
+                    str(alias)],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+                stdout,stderr = MyOut.communicate()
+                await bot.send_message(discord.Object(id=bot_channel), stdout.decode("utf-8") )
+        else:
+            MyOut = subprocess.Popen(['ideviceinstaller', '-u', devices.get(name), '--uninstall',
+                str(alias)],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+            stdout,stderr = MyOut.communicate()
+            await bot.send_message(discord.Object(id=bot_channel), stdout.decode("utf-8") )
+    except:    
+        await bot.send_message(discord.Object(id=bot_channel), "Error in device name or app not found.")
 
 @bot.command(pass_context=True)
 async def mac(ctx):
